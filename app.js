5b9f274bf102a100b9bbc7ad45c877002f286e42
Adryanssyah <adryansyah434@gmail.com>
2023-12-06 23:13:18 +0700

    test 5

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-// const express = require('express');
-// require('dotenv').config();
-// const cors = require('cors');
-// const router = require('./routes/routes');
-// const bodyParser = require('body-parser');
-// const session = require('express-session');
-// const cookieParser = require('cookie-parser');
-// const MongoStore = require('connect-mongo');
-// const mongoose = require('mongoose');
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const router = require('./routes/routes');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const mongoose = require('mongoose');
 
-// const app = express();
-// app.use(cookieParser());
-// app.use(
-//      cors({
-//           origin: process.env.CLIENT,
-//           credentials: true,
-//      })
-// );
+const app = express();
+app.use(cookieParser());
+app.use(
+     cors({
+          origin: process.env.CLIENT,
+          credentials: true,
+     })
+);
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(
-//      session({
-//           secret: 'rahasia',
-//           resave: false,
-//           saveUninitialized: false,
-//           store: MongoStore.create({ mongoUrl: process.env.MONGO_URL, collectionName: 'sessions' }),
-//           cookie: {
-//                expires: new Date(Date.now() + 30 * 86400 * 1000),
-//                maxAge: 30 * 86400 * 1000,
-//                sameSite: false,
-//                httpOnly: false,
-//           },
-//      })
-// );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+     session({
+          secret: 'rahasia',
+          resave: false,
+          saveUninitialized: false,
+          store: MongoStore.create({ mongoUrl: process.env.MONGO_URL, collectionName: 'sessions' }),
+          cookie: {
+               expires: new Date(Date.now() + 30 * 86400 * 1000),
+               maxAge: 30 * 86400 * 1000,
+               sameSite: false,
+               httpOnly: false,
+          },
+     })
+);
 
 // if (app.get('env') === 'production') {
 //      app.set('trust proxy', 1);
 //      sess.cookie.secure = true;
 // }
 
-// app.use(router);
-
-// module.exports = app;
-
-const app = require('express')();
-const { v4 } = require('uuid');
-
-app.get('/checkSession', (req, res) => {
-     res.send(false);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-     const { slug } = req.params;
-     res.end(`Item: ${slug}`);
-});
+app.use(router);
 
 module.exports = app;
+
+// const app = require('express')();
+// const { v4 } = require('uuid');
+
+// app.get('/checkSession', (req, res) => {
+//      res.send(false);
+// });
+
+// app.get('/api/item/:slug', (req, res) => {
+//      const { slug } = req.params;
+//      res.end(`Item: ${slug}`);
+// });
+
+// module.exports = app;
