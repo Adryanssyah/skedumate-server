b89785388fe71247c317ffde5d0f4d2dd06f5b16
Adryanssyah <adryansyah434@gmail.com>
2023-12-06 22:59:52 +0700

    Test

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const router = require('./routes/routes');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const MongoStore = require('connect-mongo');
-const mongoose = require('mongoose');
+// const express = require('express');
+// require('dotenv').config();
+// const cors = require('cors');
+// const router = require('./routes/routes');
+// const bodyParser = require('body-parser');
+// const session = require('express-session');
+// const cookieParser = require('cookie-parser');
+// const MongoStore = require('connect-mongo');
+// const mongoose = require('mongoose');
 
-const app = express();
-app.use(cookieParser());
-app.use(
-     cors({
-          origin: process.env.CLIENT,
-          credentials: true,
-     })
-);
+// const app = express();
+// app.use(cookieParser());
+// app.use(
+//      cors({
+//           origin: process.env.CLIENT,
+//           credentials: true,
+//      })
+// );
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(
-     session({
-          secret: 'rahasia',
-          resave: false,
-          saveUninitialized: false,
-          store: MongoStore.create({ mongoUrl: process.env.MONGO_URL, collectionName: 'sessions' }),
-          cookie: {
-               expires: new Date(Date.now() + 30 * 86400 * 1000),
-               maxAge: 30 * 86400 * 1000,
-               sameSite: false,
-               httpOnly: false,
-          },
-     })
-);
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(
+//      session({
+//           secret: 'rahasia',
+//           resave: false,
+//           saveUninitialized: false,
+//           store: MongoStore.create({ mongoUrl: process.env.MONGO_URL, collectionName: 'sessions' }),
+//           cookie: {
+//                expires: new Date(Date.now() + 30 * 86400 * 1000),
+//                maxAge: 30 * 86400 * 1000,
+//                sameSite: false,
+//                httpOnly: false,
+//           },
+//      })
+// );
 
-if (app.get('env') === 'production') {
-     app.set('trust proxy', 1);
-     sess.cookie.secure = true;
-}
+// if (app.get('env') === 'production') {
+//      app.set('trust proxy', 1);
+//      sess.cookie.secure = true;
+// }
 
-app.use(router);
+// app.use(router);
+
+// module.exports = app;
+
+const app = require('express')();
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+     const path = `/api/item/${v4()}`;
+     res.setHeader('Content-Type', 'text/html');
+     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+     const { slug } = req.params;
+     res.end(`Item: ${slug}`);
+});
 
 module.exports = app;
