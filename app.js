586378efe05dc2ab96e6589042686778b88562bd
Adryanssyah <adryansyah434@gmail.com>
2023-12-06 22:41:14 +0700

    serverless app

--- app.js
+++ app.js
@@ -41,7 +41,4 @@ if (app.get('env') === 'production') {
 
 app.use(router);
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-     console.log(`listening on ${port}`);
-});
+export default app;
