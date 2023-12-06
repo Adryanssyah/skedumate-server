96cf2755640ac20ac18ee22ab8c949733d46e82a
Adryanssyah <adryansyah434@gmail.com>
2023-12-06 22:53:55 +0700

    fix4

--- app.js
+++ app.js
@@ -41,8 +41,4 @@ if (app.get('env') === 'production') {
 
 app.use(router);
 
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-     console.log(`listening on ${port}`);
-});
+module.exports = app;
